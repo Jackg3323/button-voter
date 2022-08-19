a28b7da347cb8240c1b8a63751e63d9d386218d9
Manav Misra <manavm@visionwebsoft.com>
2022-08-19 11:41:32 -0500

    Add Profile snapshot test

--- src/components/Profile/Profile.test.jsx
+++ src/components/Profile/Profile.test.jsx
@@ -1,1 +1,10 @@
-// TODO: Write a snapshot test and visually inspect it to insure that it is rendering correctly
+import renderer from "react-test-renderer";
+import Profile from "./Profile";
+
+it("renders without crashing", () => {
+  const rendered = renderer
+    .create(<Profile name="Mark" imgUrl="someImage.jpg" />)
+    .toJSON();
+
+  expect(rendered).toMatchSnapshot();
+});

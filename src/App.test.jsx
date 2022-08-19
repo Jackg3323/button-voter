b0719ef2a999eb5f15488056f9a6c25e114abe11
Manav Misra <manavm@visionwebsoft.com>
2022-08-19 12:06:31 -0500

    Add App test to verify Profile

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -8,17 +8,22 @@ it("renders without crashing", () => {
   expect(rendered).toMatchSnapshot();
 });
 
-it("updates the name in the header whenever we submit the form", () => {
+it("displays the name and profile whenever we submit the form", () => {
   const nameInput = "John Doe";
 
   render(<App />);
 
   const h1 = screen.getByRole("heading", { level: 1 });
   const input = screen.getByLabelText(/name/i);
-  const submitBtn = screen.getByRole("button");
+  const submitBtn = screen.getByRole("button", { name: /submit/i });
 
   userEvent.type(input, nameInput);
   userEvent.click(submitBtn);
 
+  // Be sure to look for this ONLY after we have submitted the form.
+  const h2 = screen.getByRole("heading", { level: 2 });
+  screen.getByAltText(nameInput);
+
   expect(h1.textContent).toContain(nameInput);
+  expect(h2.textContent).toContain(nameInput);
 });

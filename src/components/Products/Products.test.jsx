df32b00f46d8a7f8e8624873d3e86d98ed8a1365
Manav Misra <manavm@visionwebsoft.com>
2022-08-19 09:45:43 -0500

    Add Products snapshot test

--- src/components/Products/Products.test.jsx
+++ src/components/Products/Products.test.jsx
@@ -1,17 +1,15 @@
+import renderer from "react-test-renderer";
+import Products from "./Products";
+
 // Test this component with dummy/mock data
 const products = [
   {
     price: "$49.99",
     name: "Test 1",
   },
-  {
-    price: "$9.99",
-    name: "Test 2",
-  },
-  {
-    price: "$29.99",
-    name: "Test 3",
-  },
 ];
 
-// TODO: Write a snapshot test and visually inspect it to insure that it is rendering correctly
+it("renders without crashing", () => {
+  const rendered = renderer.create(<Products products={products} />).toJSON();
+  expect(rendered).toMatchSnapshot();
+});
